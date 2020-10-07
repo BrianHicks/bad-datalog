afc81a65cb6456ab23d6a234190502b87c23933c
Brian Hicks <brian@brianthicks.com>
2020-10-07 17:09:02 -0500

    modeling based on text definitions

---
+++ src/Datalog.elm
@@ -0,0 +1,50 @@
+module Datalog exposing (..)
+
+import Sort exposing (Sorter)
+
+
+type Program
+    = Program (List Rule)
+
+
+type Rule
+    = Rule Atom (List Atom)
+
+
+type Atom
+    = Atom String (List Term)
+
+
+type Term
+    = Constant String
+    | Variable String
+
+
+{-| This is cheating a bit. A database is only ground atoms--that is, atoms
+whose terms are all constants.
+-}
+type Database
+    = Database (List Atom)
+
+
+
+-- EXAMPLES
+
+
+allPairsReachability : Program
+allPairsReachability =
+    Program
+        [ Rule
+            (Atom "reachable" [ Variable "X", Variable "Y" ])
+            [ Atom "link" [ Variable "X", Variable "Y" ] ]
+        , Rule
+            (Atom "reachable" [ Variable "X", Variable "Y" ])
+            [ Atom "link" [ Variable "X", Variable "Z" ]
+            , Atom "reachable" [ Variable "Z", Variable "Y" ]
+            ]
+
+        -- query
+        , Rule
+            (Atom "query" [ Variable "X", Variable "Y" ])
+            [ Atom "reachable" [ Variable "X", Variable "Y" ] ]
+        ]

47035785ab04f86477c8ea87a76bea09b29fe0c3
Brian Hicks <brian@brianthicks.com>
2020-10-08 16:35:23 -0500

    add atom unification

--- src/Datalog.elm
+++ src/Datalog.elm
@@ -1,5 +1,6 @@
 module Datalog exposing (..)
 
+import Dict exposing (Dict)
 import Sort exposing (Sorter)
 
 
@@ -25,6 +26,59 @@ whose terms are all constants.
 -}
 type Database
     = Database (List Atom)
+
+
+
+-- EVALUATION
+
+
+type alias Substitutions =
+    Dict String String
+
+
+unify : Atom -> Atom -> Maybe Substitutions
+unify (Atom aName aTerms) (Atom bName bTerms) =
+    if aName == bName && List.length aTerms == List.length bTerms then
+        unifyHelp (List.map2 Tuple.pair aTerms bTerms) Dict.empty
+
+    else
+        Nothing
+
+
+unifyHelp : List ( Term, Term ) -> Substitutions -> Maybe Substitutions
+unifyHelp termPairs substitutions =
+    let
+        variableToConstant var const rest =
+            case Dict.get var substitutions of
+                Nothing ->
+                    unifyHelp rest (Dict.insert var const substitutions)
+
+                Just alreadyBound ->
+                    if alreadyBound == const then
+                        unifyHelp rest substitutions
+
+                    else
+                        Nothing
+    in
+    case termPairs of
+        [] ->
+            Just substitutions
+
+        ( Constant a, Constant b ) :: rest ->
+            if a == b then
+                unifyHelp rest substitutions
+
+            else
+                Nothing
+
+        ( Variable var, Constant const ) :: rest ->
+            variableToConstant var const rest
+
+        ( Constant const, Variable var ) :: rest ->
+            variableToConstant var const rest
+
+        ( Variable _, Variable _ ) :: rest ->
+            unifyHelp rest substitutions
 
 
 
---
+++ tests/DatalogTest.elm
@@ -0,0 +1,95 @@
+module DatalogTest exposing (..)
+
+import Datalog exposing (..)
+import Dict
+import Expect
+import Test exposing (..)
+
+
+unifyTest : Test
+unifyTest =
+    describe "unify"
+        [ test "atoms with different names do not unify" <|
+            \_ ->
+                unify (Atom "a" []) (Atom "b" [])
+                    |> Expect.equal Nothing
+        , test "atoms with different arities do not unify" <|
+            \_ ->
+                unify
+                    (Atom "a" [ Variable "A" ])
+                    (Atom "a" [ Variable "A", Variable "B" ])
+                    |> Expect.equal Nothing
+        , test "conflicting constants do not unify" <|
+            \_ ->
+                unify
+                    (Atom "a" [ Constant "x" ])
+                    (Atom "a" [ Constant "y" ])
+                    |> Expect.equal Nothing
+        , test "compatible constants unify" <|
+            \_ ->
+                unify
+                    (Atom "a" [ Constant "x" ])
+                    (Atom "a" [ Constant "x" ])
+                    |> Expect.equal (Just Dict.empty)
+        , test "a unbound var/constant pair unifies" <|
+            \_ ->
+                unify
+                    (Atom "a" [ Variable "X" ])
+                    (Atom "a" [ Constant "a" ])
+                    |> Expect.equal (Just (Dict.singleton "X" "a"))
+        , test "a bound var/constant pair unifies if it does not conflict" <|
+            \_ ->
+                unify
+                    (Atom "a" [ Variable "X", Variable "X" ])
+                    (Atom "a" [ Constant "a", Constant "a" ])
+                    |> Expect.equal (Just (Dict.singleton "X" "a"))
+        , test "a constant/bound var pair does not unify if it conflicts" <|
+            \_ ->
+                unify
+                    (Atom "a" [ Variable "X", Variable "X" ])
+                    (Atom "a" [ Constant "a", Constant "b" ])
+                    |> Expect.equal Nothing
+        , test "a constant/unbound var pair unifies" <|
+            \_ ->
+                unify
+                    (Atom "a" [ Constant "a" ])
+                    (Atom "a" [ Variable "X" ])
+                    |> Expect.equal (Just (Dict.singleton "X" "a"))
+        , test "a constant/bound var pair unifies if it does not conflict" <|
+            \_ ->
+                unify
+                    (Atom "a" [ Constant "a", Constant "a" ])
+                    (Atom "a" [ Variable "X", Variable "X" ])
+                    |> Expect.equal (Just (Dict.singleton "X" "a"))
+        , test "a bound var/constant pair does not unify if it conflicts" <|
+            \_ ->
+                unify
+                    (Atom "a" [ Constant "a", Constant "b" ])
+                    (Atom "a" [ Variable "X", Variable "X" ])
+                    |> Expect.equal Nothing
+        , test "variables unify with each other but don't generate any bindings" <|
+            \_ ->
+                unify
+                    (Atom "a" [ Variable "X" ])
+                    (Atom "a" [ Variable "Y" ])
+                    |> Expect.equal (Just Dict.empty)
+        , test "more than one variable can be bound in an atom" <|
+            \_ ->
+                unify
+                    (Atom "a" [ Variable "X", Variable "Y" ])
+                    (Atom "a" [ Constant "a", Constant "b" ])
+                    |> Expect.equal
+                        (Just
+                            (Dict.fromList
+                                [ ( "X", "a" )
+                                , ( "Y", "b" )
+                                ]
+                            )
+                        )
+        , test "multiple variables can cause conflicts which fail to unify" <|
+            \_ ->
+                unify
+                    (Atom "a" [ Variable "X", Variable "X" ])
+                    (Atom "a" [ Constant "a", Constant "b" ])
+                    |> Expect.equal Nothing
+        ]

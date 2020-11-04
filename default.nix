b2b614a471b43daa9a3807cf3515e32fc09921de
Brian Hicks <brian@brianthicks.com>
2020-11-04 13:43:02 -0600

    build a docker container

--- default.nix
+++ default.nix
@@ -1,29 +1,41 @@
 { ... }:
 let
   sources = import ./nix/sources.nix;
-  nixpkgs = import sources.nixpkgs { };
+  pkgs = import sources.nixpkgs { };
   gitignore = import sources.gitignore { };
-in with nixpkgs;
-stdenv.mkDerivation {
-  name = "datalog";
-  src = gitignore.gitignoreSource ./.;
+in rec {
+  datalog = pkgs.stdenv.mkDerivation {
+    name = "datalog";
+    src = gitignore.gitignoreSource ./.;
 
-  buildInputs = [ elmPackages.elm elmPackages.elm-test ];
-  buildPhase = pkgs.elmPackages.fetchElmDeps {
-    elmPackages = import ./nix/elm-srcs.nix;
-    elmVersion = "0.19.1";
-    registryDat = ./nix/registry.dat;
+    buildInputs = [ pkgs.elmPackages.elm pkgs.elmPackages.elm-test ];
+    buildPhase = pkgs.elmPackages.fetchElmDeps {
+      elmPackages = import ./nix/elm-srcs.nix;
+      elmVersion = "0.19.1";
+      registryDat = ./nix/registry.dat;
+    };
+
+    doCheck = true;
+    checkPhase = ''
+      elm-test
+    '';
+
+    installPhase = ''
+      mkdir -p $out/share/datalog
+      cd playground
+      elm make --optimize --output $out/share/datalog/index.html src/Main.elm
+    '';
   };
 
-  doCheck = true;
-  checkPhase = ''
-    elm-test
-  '';
-
-  installPhase = ''
-    mkdir -p $out/share/datalog
-    cd playground
-    elm make --optimize --output $out/share/datalog/index.html src/Main.elm
-  '';
+  container = let
+    linuxPkgs = import sources.nixpkgs { system = "x86_64-linux"; };
+    listenPort = "80";
+  in linuxPkgs.dockerTools.buildLayeredImage {
+    name = "datalog";
+    contents = linuxPkgs.darkhttpd;
+    config = {
+      Cmd = [ "darkhttpd" "${datalog}/share/datalog" ];
+      ExposedPorts = { "${listenPort}/tcp" = { }; };
+    };
+  };
 }
-

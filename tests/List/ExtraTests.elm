d3aa0e8fbe8507e8cec177dcd838e4183d4385c5
Brian Hicks <brian@brianthicks.com>
2021-05-05 06:15:40 -0500

    test foldrResult

--- tests/List/ExtraTests.elm
+++ tests/List/ExtraTests.elm
@@ -1,6 +1,7 @@
 module List.ExtraTests exposing (..)
 
 import Expect
+import Fuzz
 import List.Extra exposing (..)
 import Test exposing (..)
 
@@ -23,4 +24,22 @@ indexOfTests =
                 [ 1 ]
                     |> indexOf 2
                     |> Expect.equal Nothing
+        ]
+
+
+foldrResultTests : Test
+foldrResultTests =
+    describe "foldrResult"
+        [ test "works like a foldr when the function always returns OK" <|
+            \_ ->
+                foldrResult (\a b -> Ok (a + b)) 0 [ 1, 2, 3 ]
+                    |> Expect.equal (Ok (List.foldr (+) 0 [ 1, 2, 3 ]))
+        , test "returns an error when the provided function does" <|
+            \_ ->
+                foldrResult (\_ _ -> Err ()) () [ 1 ]
+                    |> Expect.err
+        , fuzz Fuzz.int "always returns the initial value for an empty list" <|
+            \initial ->
+                foldrResult (\_ _ -> Err ()) initial []
+                    |> Expect.equal (Ok initial)
         ]

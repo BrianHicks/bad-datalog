e9b5c4d75615ec70c713e36feb58bc0513a689e4
Brian Hicks <brian@brianthicks.com>
2021-05-13 16:33:27 -0500

    run elm-test

--- default.nix
+++ default.nix
@@ -5,13 +5,16 @@ in nixpkgs.stdenv.mkDerivation {
   name = "bad-datalog";
 
   src = gitignore.gitignoreSource ./.;
-  buildInputs = [ nixpkgs.elmPackages.elm ];
+  buildInputs = [ nixpkgs.elmPackages.elm nixpkgs.elmPackages.elm-test ];
 
   buildPhase = nixpkgs.elmPackages.fetchElmDeps {
     elmPackages = import ./nix/elm-srcs.nix;
     elmVersion = "0.19.1";
     registryDat = ./nix/registry.dat;
   };
+
+  doCheck = true;
+  checkPhase = "elm-test";
 
   # it's a little weird that elm2nix says it's fine to build in the install
   # phase, but who am I to judge?
